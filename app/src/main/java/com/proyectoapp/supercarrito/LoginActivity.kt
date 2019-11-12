7a8146d70ad513af12fbb71b92ad590ba4cefed2
Leonardo Muñoz Veloso <leonardoeleazar.mv@gmail.com>
2019-11-12 06:53:53 -0300

    Login y SplashScreen listos

---
+++ app/src/main/java/com/proyectoapp/supercarrito/LoginActivity.kt
@@ -0,0 +1,70 @@
+package com.proyectoapp.supercarrito
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
+import android.widget.EditText
+import android.widget.ProgressBar
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var txtUser: EditText
+    private lateinit var txtPass: EditText
+    private lateinit var progressBar: ProgressBar
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        //quitar Titulo de app de la parte superior  --Leonardo
+        supportActionBar?.hide()
+
+
+        txtUser=findViewById(R.id.txtUser)
+        txtPass=findViewById(R.id.txtPass)
+        progressBar= findViewById(R.id.progressBar)
+
+
+        auth= FirebaseAuth.getInstance()
+    }
+
+    fun forgotPassword(@Suppress("UNUSED_PARAMETER")view: View){
+        startActivity(Intent(this,ForgotPassActivity::class.java))
+    }
+
+    fun register(@Suppress("UNUSED_PARAMETER")view: View){
+        startActivity(Intent(this, RegisterActivity::class.java))
+    }
+
+    fun login(@Suppress("UNUSED_PARAMETER")view: View){
+        loginUser()
+    }
+    private fun loginUser(){
+        val user:String=txtUser.text.toString()
+        val pass:String=txtPass.text.toString()
+
+        if (!TextUtils.isEmpty(user) && !TextUtils.isEmpty(pass)){
+            progressBar.visibility=View.VISIBLE
+
+            auth.signInWithEmailAndPassword(user,pass)
+                .addOnCompleteListener(this){
+                    task ->
+                    if (task.isSuccessful){
+                        action()
+                    } else{
+                        Toast.makeText(this,"Error en la autenticación", Toast.LENGTH_LONG).show()
+
+                    }
+                }
+        }
+    }
+
+    private fun action(){
+        startActivity(Intent(this, MainActivity::class.java))
+    }
+}
---
+++ app/src/main/java/com/proyectoapp/supercarrito/SplashActivity.kt
@@ -0,0 +1,22 @@
+package com.proyectoapp.supercarrito
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        //quitar Titulo de app de la parte superior  --Leonardo
+        supportActionBar?.hide()
+
+        //El logo de la app es provisorio, se puede cambiar mas adelante
+
+        //Abrir activity Login despues de 2 segundos
+         Handler().postDelayed(Runnable { startActivity(Intent(this,LoginActivity::class.java)) },2000)
+
+    }
+}
