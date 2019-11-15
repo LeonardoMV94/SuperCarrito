209f6f8fe4dfbfc0a16b8ebb382e98591e992cc9
Leonardo Muñoz Veloso <leonardoeleazar.mv@gmail.com>
2019-11-15 20:04:08 -0300

    Camdio de ideas

---
+++ app/src/main/java/com/proyectoapp/supercarrito/vista/entrada/EntradaActivity.java
@@ -0,0 +1,16 @@
+package com.proyectoapp.supercarrito.vista.entrada;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.proyectoapp.supercarrito.R;
+
+public class EntradaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_escaneo);
+    }
+}
---
+++ app/src/main/java/com/proyectoapp/supercarrito/vista/ventas/VentaActivity.java
@@ -0,0 +1,16 @@
+package com.proyectoapp.supercarrito.vista.ventas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.proyectoapp.supercarrito.R;
+
+public class VentaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_venta);
+    }
+}
